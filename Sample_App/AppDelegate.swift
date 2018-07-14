a5d50aac143a9728e97e3a78b46dd9b0e84fc8ba
Ashish <ashish.june17@gmail.com>
2018-07-14 10:58:55 +0530

    Second commit

--- Sample_App/AppDelegate.swift
+++ Sample_App/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("c49a4d02-c1c0-4fec-a66f-3a069723ddbb", withServices:[ MSAnalytics.self, MSCrashes.self ])
         return true
     }
 
